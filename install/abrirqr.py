1c7df96aec4a112d8ac87196a19465ba6f52f74f
CanalQb <qrodrigob@gmail.com>
2023-07-21 19:44:19 -0300

    Update abrirqr.py

--- install/abrirqr.py
+++ install/abrirqr.py
@@ -3,6 +3,29 @@ import os
 import telethon
 from telethon import TelegramClient
 from qrcode import QRCode
+qr = QRCode()
+def gen_qr(token:str):
+    qr.clear()
+    qr.add_data(token)
+    qr.print_ascii()
+def display_url_as_qr(url):
+    print(url)  # faça o que for necessário para mostrar a URL como um QR para o usuário
+    gen_qr(url)
+async def main(client: telethon.TelegramClient):
+    if(not client.is_connected()): # se o cliente ainda não estiver conectado
+        await client.connect() # conecte-se
+    client.connect() # conecte-se novamente
+    qr_login = await client.qr_login() # inicie o login do QR
+    print(client.is_connected())
+    r = False
+    while not r:
+        display_url_as_qr(qr_login.url) # mostre o QR
+        # Importante! Você precisa esperar o login ser concluído!
+        try:
+            r = await qr_login.wait(10) # aguarda até que o login seja concluído ou que passe 10 segundos
+        except:
+            await qr_login.recreate() # se não tiver sucesso, reinicie o processo de login
+            
 import platform
 
 sistema_operacional = platform.system()
@@ -17,7 +40,6 @@ else:
     idsystem = 'o'
     
 #idsystem Pode retornar w para Windows), (a para Android), (l para Linux), )o para outros sistemas)
-print('ddd',DDIDDDTELEFONE)
 
 diretorio_atual = os.getcwd()
 caminho_arquivo = os.path.join(diretorio_atual, DDIDDDTELEFONE, "dados", "meutelefone.txt")
@@ -38,12 +60,8 @@ with open(caminho_arquivo, 'r') as arquivo:
             hash_ = linha.split()[1]
 
 # Imprimir as variáveis extraídas
-#print("TEL:", tel)
-#print("ID:", id_)
-#print("HASH:", hash_)
-
 session_file = os.path.join(idsystem + tel) 
 print(session_file)
 client = TelegramClient(session_file, int(id_), str(hash_), device_model= 'bot', timeout=2, connection_retries=1, auto_reconnect=True)
-#client.loop.run_until_complete(main(client))
-#client.disconnect()
+client.loop.run_until_complete(main(client))
+client.disconnect()
