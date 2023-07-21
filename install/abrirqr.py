bfa6db378e7ed6931a6e574a99265a5fbfb4e56a
CanalQb <qrodrigob@gmail.com>
2023-07-21 16:43:04 -0300

    Update abrirqr.py

--- install/abrirqr.py
+++ install/abrirqr.py
@@ -1,5 +1,6 @@
 import telethon
 from qrcode import QRCode
+import os
 import platform
 
 sistema_operacional = platform.system()
@@ -15,3 +16,9 @@ else:
 #idsystem Pode retornar w para Windows), (a para Android), (l para Linux), )o para outros sistemas)
 print('ddd',DDIDDDTELEFONE)
 print('telefone', telefone)
+
+diretorio_atual = os.getcwd()
+diretorio_pai = os.path.dirname(diretorio_atual)
+
+print("O diretorio_atual é:", diretorio_atual)
+print("O diretório do nível anterior é:", diretorio_pai)
