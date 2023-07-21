88f9a3e07fd834ef40bc8309b666f190c52e9c12
CanalQb <qrodrigob@gmail.com>
2023-07-21 16:20:10 -0300

    Update abrirqr.py

--- install/abrirqr.py
+++ install/abrirqr.py
@@ -13,20 +13,5 @@ elif sistema_operacional == "Linux":
 else:
     idsystem = 'o'
 #idsystem Pode retornar w para Windows), (a para Android), (l para Linux), )o para outros sistemas)
-
-#______Resgatando Variaveis ______
-api_id = id_telegram  # Substitua pelo seu API ID
-api_hash = hash_telegram  # Substitua pelo seu API Hash
-telefone = str(DDIDDDTELEFONE)
-session_file = os.path.join(idsystem + telefone)
-
-nomedevice = idsystem + telefone
-client = TelegramClient(session_file, int(api_id), str(api_hash), device_model= 'bot', timeout=2, connection_retries=1, auto_reconnect=True)
-client.loop.run_until_complete(main(client))
-client.disconnect()
-
-# Obtendo o nome da sessão
-#session_name = os.path.splitext(session_file)[0]
-
-client = locals().get('client')
-print("Arquivo de saída gerado com sucesso!")
+print(DDIDDDTELEFONE)
+print(telefone)
